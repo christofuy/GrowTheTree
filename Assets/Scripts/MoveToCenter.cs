1074399bae9a476fc36daedcd4b52338b86ea4f8
willtylerjr <willtylerjr@outlook.com>
2022-04-21 18:45:08 -0700

    MoveToCenter Works
    MoveToCenter.cs when applied to any gameObject will have that object slowly creep towards the tree. This will likely become a method in a Termite class, but for now, this works.

---
+++ Assets/Scripts/MoveToCenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToCenter : MonoBehaviour
+{
+    public GameObject tree;
+    public float speed = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, tree.transform.position, speed * Time.deltaTime);
+    }
+}
