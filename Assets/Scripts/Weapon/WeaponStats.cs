871e8a3b766dd95490ac5420303cedf5f4dbd4ec
Christopher <cyoun@chapman.edu>
2022-04-22 21:22:50 -0700

    feat: Create WeaponStats class

---
+++ Assets/Scripts/Weapon/WeaponStats.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public enum WeaponType {MELEE, RANGED};
+
+[CreateAssetMenu(fileName = "New Weapon Stats", menuName = "WeaponStats")]
+public class WeaponStats : ScriptableObject
+{
+    new public string name = "Weapon Name";
+    public WeaponType type;
+    public Sprite sprite = null;
+    public float meleeAttackRange = 0.5f;
+    public float projectileForce = 20f;
+    public GameObject projectilePrefab;
+}
