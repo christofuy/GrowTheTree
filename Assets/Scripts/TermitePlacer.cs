e2dfd9538eb9a010cf0c26d600cd828bc4c034a3
willtylerjr <willtylerjr@outlook.com>
2022-04-26 16:11:39 -0700

    Placer Places Termites (Set to 5)

---
+++ Assets/Scripts/TermitePlacer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TermitePlacer : MonoBehaviour
+{
+
+    public GameObject TermitePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Place(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector3 RandomPosition()
+    {
+        float min = 0.0f;
+        float max = 1f;
+
+        Vector3 final = Camera.main.ViewportToWorldPoint(new Vector3(Random.Range(min, max), 1, 10));
+        return final;
+    }
+
+
+    public void Place(int limit)
+    {
+
+        int i = 0;
+        while (i < limit)
+        {
+            Instantiate(TermitePrefab, RandomPosition(), Quaternion.identity);
+            i++;
+        }
+        
+    }
+}
