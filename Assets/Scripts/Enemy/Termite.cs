f21dda7a780d1d0bbdfa8fc2d8554c5b499895d4
willtylerjr <willtylerjr@outlook.com>
2022-04-28 17:15:55 -0700

    WIP Termite Attacks & Tree Health
    see title

--- Assets/Scripts/Enemy/Termite.cs
+++ Assets/Scripts/Enemy/Termite.cs
@@ -7,8 +7,16 @@ public class Termite : Enemy
     public GameObject tree;
     public SpriteRenderer spriteRenderer;
     public float speed = 1f;
+    private int attackCoolDownTimeSeconds = 2;
+
+
+    public LayerMask opposingLayers;
+    public Weapon weapon;
+
+    bool canAttack = false;
 
     Vector3 target;
+
 
     void Start()
     {
@@ -19,7 +27,27 @@ public class Termite : Enemy
     {
         MoveToTarget(target);
         Rotate(transform.position);
+        if (canAttack)
+        {
+            print("termite attacked!");
+            //weapon.Attack(opposingLayers);
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        canAttack = true;
+        StartCoroutine(AttackCooldown());
+        //Start coRoutine timer to make canAttack false after a moment.
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(attackCoolDownTimeSeconds);
+        canAttack = false;
+    }
+
+    //Need a function that sets "canAttack" to false after two? seconds if canAttack is made true
 
 
     private void MoveToTarget(Vector3 target)
