4ddfdce42ad521668deb8efcab7d17119b4c3913
willtylerjr <willtylerjr@outlook.com>
2022-05-12 17:00:45 -0700

    WIP UI Elements for Slider Bars
    working on the code, just need to wire up and finalize where it grabs the numbers

---
+++ Assets/Scripts/GameLogic/User Interface/UI Elements for Bars.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElementsforBars : MonoBehaviour
+{
+    public TreeHealthBar TreeHealthBar;
+    public MagicBar MagicBar;
+    public PlayerHealthBar PlayerHealthBar;
+
+    public Text text_TreeHealth;
+    public Text text_PlayerMagic;
+    public Text text_PlayerHealth;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text_TreeHealth.text = "" + TreeHealthBar.currentTreeHealth;
+        text_PlayerMagic.text = "";
+        text_PlayerHealth.text = "";
+    }
+}
